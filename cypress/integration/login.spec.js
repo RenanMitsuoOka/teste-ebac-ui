f7551c6f505c6b6356252a0cfcbeed5d0df51f97
Renan Mitsuo <renan@softeasy.com.br>
2023-10-25 21:09:34 -0300

    Meu segundo commit

--- cypress/integration/login.spec.js
+++ cypress/integration/login.spec.js
@@ -1,25 +1,28 @@
 /// <reference types="Cypress" />
 
 context('Funcionalidade Login',() => {
+    beforeEach(() =>{
+        cy.visit('http://lojaebac.ebaconline.art.br/minha-conta/')
+    });
+    ///tira print das evidencias
+    ///afterEach(() =>{
+    ///    cy.screenshot()
+    ///})
 
-})
 it('Deve fazer login com sucesso',() =>{
-    cy.visit('http://lojaebac.ebaconline.art.br/minha-conta/')
     cy.get('#username').type('aluno_ebac@teste.com')
     cy.get('#password').type('teste@teste.com')
     cy.get('.woocommerce-form > .button').click()
     cy.get('.woocommerce-MyAccount-navigation-link--customer-logout > a').should('contain', 'Sair')    
 })
 it('Deve exibir uma mensagem de erro ao invesir usuario invalidos',() =>{
-    cy.visit('http://lojaebac.ebaconline.art.br/minha-conta/')
     cy.get('#username').type('loginerrado@teste.com')
     cy.get('#password').type('teste@test')
     cy.get('.woocommerce-form > .button').click()
 })
 it('Deve exibir uma mensagem de erro ao invesir senha invalidos',() =>{
-    cy.visit('http://lojaebac.ebaconline.art.br/minha-conta/')
     cy.get('#username').type('aluno_ebac@teste.com')
     cy.get('#password').type('teste@test')
     cy.get('.woocommerce-form > .button').click()
     cy.get('.woocommerce-error > li').should('contain', 'Erro')
-})
+})});
---
+++ cypress/integration/produtos.speck.js
@@ -0,0 +1,8 @@
+///<reference types = "cypress" />
+
+
+describe('Funcionalidade pagina de produtos', () =>{
+    beforeEach(() =>{
+        cy.visit('http://lojaebac.ebaconline.art.br/produtos/')
+    });
+});
