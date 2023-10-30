273a61806f402130a13abb8402c70ca879618b76
Renan Mitsuo <renan@softeasy.com.br>
2023-10-30 19:01:47 -0300

    Alterações dos exercicios

--- cypress/integration/login.spec.js
+++ cypress/integration/login.spec.js
@@ -1,4 +1,5 @@
 /// <reference types="Cypress" />
+///npx cypress open => abre o cypress
 
 context('Funcionalidade Login',() => {
     beforeEach(() =>{
--- cypress/integration/produtos.speck.js
+++ cypress/integration/produtos.speck.js
@@ -1,8 +1,29 @@
 ///<reference types = "cypress" />
 
-
-describe('Funcionalidade pagina de produtos', () =>{
-    beforeEach(() =>{
+describe('Funcionalidade pagina de Produto', () => {
+    beforeEach(() => {
         cy.visit('http://lojaebac.ebaconline.art.br/produtos/')
     });
-});
+
+    it('Deve Selecionar um produto da lista', () => {
+        cy.get('[class="product-block grid"]')
+            //.first()
+            //.last()
+            //Pega 4 item da lista
+            //.eq(3)
+            .contains('Bruno Compete Hoodie')
+            .click()
+    });
+    it.only('Deve adicionar um produto ao carrinho',() =>{
+        var quantidade = 7
+        cy.get('[class="product-block grid"]')
+        .contains('Bruno Compete Hoodie').click()
+        cy.get('.button-variable-item-S').click()
+        cy.get('.button-variable-item-Black').click()
+        cy.get('.input-text')
+        .clear()
+        .type(quantidade)
+        cy.get('.single_add_to_cart_button').click()
+        cy.get('.dropdown-toggle > .mini-cart-items').should('contain', quantidade)
+    });    
+})
