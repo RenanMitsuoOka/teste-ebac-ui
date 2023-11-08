00ba99fd6e4d641cc876516004c95fbdcfd49321
Renan Mitsuo <renan@softeasy.com.br>
2023-11-08 19:15:19 -0300

    Usando base de dados

--- cypress/integration/endereco.spec.js
+++ cypress/integration/endereco.spec.js
@@ -1,6 +1,7 @@
 /// <reference types="Cypress" />
 import enderecoPage from '../support/page-objects/endereco.page';
-import EnderecoPage from '../support/page-objects/endereco.page'
+const dadosEndereco = require('../fixtures/endereco.json')
+
 describe('Funcionalidade Endereços = Faturamento de entrega', () => {
     beforeEach(() => {
         cy.visit('minha-conta')
@@ -9,7 +10,22 @@ describe('Funcionalidade Endereços = Faturamento de entrega', () => {
         })
     });
 
-    it.only('Deve fazer cadastro de faturamento com sucesso', () => {
-        enderecoPage.editarEnderecoFaturamento()
+    it('Deve fazer cadastro de faturamento com sucesso', () => {
+        enderecoPage.editarEnderecoFaturamento('Renan', 'oka', 'OEM', 'Brasil', 'Carrao', '98', 'São Paulo', '03456-030', '27217815', 'Renan@softeasy.com.br')
+        cy.get('.woocommerce-message').should('contain', 'Endereço alterado com sucesso.')
+    });
+/// Pega o arquivo da fixture endereco.json
+    it('Deve fazer cadastro de faturamento com sucesso - Usando Arquivo', () => {
+        enderecoPage.editarEnderecoFaturamento(dadosEndereco[1].nome,
+            dadosEndereco[1].sobrenome,
+            dadosEndereco[1].empresa,
+            dadosEndereco[1].pais,
+            dadosEndereco[1].endereco,
+            dadosEndereco[1].numero,
+            dadosEndereco[1].estado,
+            dadosEndereco[1].cep,
+            dadosEndereco[1].telefone,
+            dadosEndereco[1].email)
+        cy.get('.woocommerce-message').should('contain', 'Endereço alterado com sucesso.')
     });
 });
--- cypress/support/page-objects/endereco.page.js
+++ cypress/support/page-objects/endereco.page.js
@@ -1,22 +1,22 @@
 class EnderecoPage{
 
-    editarEnderecoFaturamento(){
+    editarEnderecoFaturamento(nome, sobrenome, empresa, pais, endereco, numero, estado, cep, telefone, email){
         //Elementros mais ações
         cy.get('.woocommerce-MyAccount-navigation-link--edit-address > a').click()
         cy.get(':nth-child(1) > .title > .edit').click()
-        cy.get('#billing_first_name').clear().type('Renan')
-        cy.get('#billing_last_name').clear().type('Mitsuo')
-        cy.get('#billing_company').clear().type('Softeasy')
+        cy.get('#billing_first_name').clear().type(nome)
+        cy.get('#billing_last_name').clear().type(sobrenome)
+        cy.get('#billing_company').clear().type(empresa)
         //combobox abaixo isando inspeção de codigo do crhome
-        cy.get('#select2-billing_country-container').click().type('Brasil').get('[aria-selected="true"]').click()
-        cy.get('#billing_address_1').clear().type('Henrique Carnicelli')
-        cy.get('#billing_address_2').clear().type('93')
-        cy.get('#billing_city').clear().type('São Paulo')
+        cy.get('#select2-billing_country-container').click().type(pais).get('[aria-selected="true"]').click()
+        cy.get('#billing_address_1').clear().type(endereco)
+        cy.get('#billing_address_2').clear().type(numero)
+        cy.get('#billing_city').clear().type(estado)
         //combobox abaixo usando enter
-        cy.get('#select2-billing_state-container').click().type('São Paulo{enter}')
-        cy.get('#billing_postcode').clear().type('03456-040')
-        cy.get('#billing_phone').clear().type('11952191115')
-        cy.get('#billing_email').clear().type('rnaoka@gmail.com')
+        cy.get('#select2-billing_state-container').click().type(estado + '{enter}')
+        cy.get('#billing_postcode').clear().type(cep)
+        cy.get('#billing_phone').clear().type(telefone)
+        cy.get('#billing_email').clear().type(email)
         cy.get(':nth-child(2) > .button').click()
     }
     EditarEnderecoEntrega(){
